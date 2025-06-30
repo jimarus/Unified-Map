9956b4654b13f7c377282330d18310f893b9d489
jimarus <grzp0920@gmail.com>
2025-06-30 10:58:52 +0800

    22

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,5 +1,5 @@
 function goToMainPage(){
-    window.location.href = "/unified-map/index.html";
+    window.location.href = "../index.html";
 }
         // Function to switch between pages
         function showPage(pageId) {
